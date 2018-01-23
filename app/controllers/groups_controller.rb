941010c6c6138d370eeba3314e6000ea664ea3c3
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-01-23 16:23:12 +0900

    Group機能完成（動作のみ）

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,19 +1,33 @@
 class GroupsController < ApplicationController
 
+  def index
+  end
+
   def new
     @group = Group.new
+    @group.users << current_user
   end
 
   def create
     @group =Group.new(group_params)
-    @group.save
-    redirect_to :root
+    if @group.save
+      redirect_to :root,notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to :root, notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :members
+
+  validates :group_name,presence: true
 end
