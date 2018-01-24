2e83d0885f42d2692db1edffe4233919d723bb09
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-01-24 15:30:45 +0900

    レビュー再々修正
    1._group.scssに&使用とcolorを変数に
    2.”,”のあとに半角スペース

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-  before_action :set_group,only: [:edit,:update]
+  before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -38,7 +38,7 @@ class GroupsController < ApplicationController
   end
 
   def group_params
-    params.require(:group).permit(:name,user_ids: [])
+    params.require(:group).permit(:name, user_ids: [])
 
   end
 
