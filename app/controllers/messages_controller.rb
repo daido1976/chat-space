b565026f77707a7b1d834e91890a73102980bdf4
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-01-25 17:45:47 +0900

    controller追記
    Alert時エラーのため

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+    @messages = @group.messages.includes(:user)
     if @message.save
       redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
     else
