c35c74b982a2cfeabcfd1f93ccbe37ef55a08f1a
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-01-25 20:43:45 +0900

    controller内メソッド化

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,13 @@
 class GroupsController < ApplicationController
 
   before_action :set_group, only: [:edit, :update]
+  before_action :set_users
 
   def index
   end
 
   def new
     @group = Group.new
-    @users = User.where.not(id:current_user.id)
   end
 
   def create
@@ -21,7 +21,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @users = User.where.not(id:current_user.id)
   end
 
   def update
@@ -41,7 +40,10 @@ class GroupsController < ApplicationController
 
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
 
+  def set_users
+    @users = User.where.not(id:current_user.id)
   end
 
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,13 @@
 class MessagesController < ApplicationController
 
-  before_action :set_group
+  before_action :set_group, :set_messages, :set_members
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
-    @members = User.where(id:@group.user_ids)
   end
 
   def create
     @message = @group.messages.new(message_params)
-    @messages = @group.messages.includes(:user)
     if @message.save
       redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
     else
@@ -27,6 +24,14 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def set_messages
+    @messages = @group.messages.includes(:user)
+  end
+
+  def set_members
+    @members = User.where(id:@group.user_ids)
   end
 
 end
