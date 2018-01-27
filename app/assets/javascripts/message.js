9716c5a0228ef8773a6351ff195e4bff36f11d09
daido1976 <daidoshota@daidoushoutanoMacBook-Air.local>
2018-01-27 20:51:00 +0900

    Message表示まで（諸問題あり）
    1.画像ない時console上でerror出る
    2.Submitしても前回送信した画像データが残ってる
    3.”送信されました”flashが出ない

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,26 @@
 $(function() {
+  function buildHTML(message) {
+    var html =`
+<div class='chat-main__body--message'>
+  <div class='chat-main__body--message-user'>
+    <p>
+      ${message.user_name}
+    </p>
+  </div>
+  <div class='chat-main__body--message-time'>
+    <p>
+      ${message.time}
+    </p>
+  </div>
+  <div class='chat-main__body--message-content'>
+    <p>
+      ${message.body}
+      <img src="${message.image.url}"/>
+    </p>
+  </div>
+</div>`
+    return html;
+  }
   $("#new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +32,12 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $(".chat-main__body--message-lists").append(html)
+      $(".message-area").val("")
+      $(".submit").prop("disabled", false)
     })
   })
 })
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.body @message.body
+json.image @message.image
+json.time @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
+json.id @message.id
